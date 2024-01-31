982cad2e1bc2de7ccb0348166633d23ae56552a6
Clint171 <clintsimiyu004@gmail.com>
2024-01-31 18:46:20 +0300

    feat: added chat paragraph generation

--- frontend/resources/js/chat.js
+++ frontend/resources/js/chat.js
@@ -1,11 +1,58 @@
+const { set } = require("mongoose");
+
 function checkSignIn() {
     let token = localStorage.getItem("token");
     if (token == null || token == undefined || !token) {
         window.location.href = "Signup.html";
     }
-    else{
-        alert(token);
-    }
 }
 
 checkSignIn();
+
+function logout(){
+    localStorage.removeItem("token");
+    window.location.href = "Signup.html";
+}
+
+function sendMessage(message){
+    let token = localStorage.getItem("token");
+    let url = "https://localhost:300/chat";
+    let fetchOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({"message" : message})
+    };
+    fetch(url, fetchOptions)
+    .then(async (res)=>{
+        return await res.json();
+    })
+}
+
+function createUserMessageP(message){
+    let chatDiv = document.getElementById("chatDiv");
+    let p = document.createElement("p");
+    p.classList.add("User-cont");
+    p.innerText = message;
+    chatDiv.appendChild(p);
+}
+
+function createAIMessageP(message){
+    let chatDiv = document.getElementById("chatDiv");
+    let p = document.createElement("p");
+    p.classList.add("AI-cont");
+    p.innerText = message;
+    chatDiv.appendChild(p);
+}
+
+function processMessage(){
+    let message = document.getElementById("message").value;
+    createUserMessageP(message);
+    //sendMessage(message);
+    document.getElementById("message").value = "";
+    setTimeout(()=>{
+    createAIMessageP("As an AI, I can't understand you.");
+    }, 1000);
+}

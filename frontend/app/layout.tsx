a9f34e09ec1c74f945e7a9691e4ca80615e2fe58
Clint Simiyu <clintsimiyu004@gmail.com>
2026-05-18 18:50:52 +0300

    Modified layout with vercel analytics component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
